d43368f880590f41d9cbf62c4c8978c3698647ea
Muhammad Adil Khan <adilsziu@outlook.com>
2023-10-18 16:09:05 +0500

    Lesson First Part complete

---
+++ app/Filament/Resources/CustomerResource.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CustomerResource\Pages;
+use App\Filament\Resources\CustomerResource\RelationManagers;
+use App\Models\Customer;
+use App\Models\PipelineStage;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CustomerResource extends Resource
+{
+    protected static ?string $model = Customer::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('first_name'),
+                Forms\Components\TextInput::make('last_name'),
+                Forms\Components\TextInput::make('email')
+                    ->email(),
+                Forms\Components\TextInput::make('phone_number'),
+                // ->tel(),
+                Forms\Components\Textarea::make('description')
+                    ->columnSpanFull(),
+                Forms\Components\Select::make('lead_source_id')
+                    ->relationship('leadSource', 'name'),
+                Forms\Components\Select::make('tags')
+                    ->relationship('tags', 'name')
+                    ->multiple(),
+                Forms\Components\Select::make('pipeline_stage_id')
+                    ->relationship('pipelineStage', 'name', function ($query) {
+                        // It is important to order by position to display the correct order
+                        $query->orderBy('position', 'asc');
+                    })
+                    // We are setting the default value to the default Pipeline Stage
+                    ->default(PipelineStage::where('is_default', true)->first()?->id),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('first_name')
+                    ->label("Name")
+                    // ->formatStateUsing(function ($record) {
+                    //     return $record->first_name . " " . $record->last_name;
+                    // })
+                    ->formatStateUsing(function ($record) {
+                        $tagsList = view('customer.tagsList', ['tags' => $record->tags])->render();
+
+                        return $record->first_name . ' ' . $record->last_name . ' ' . $tagsList;
+                    })
+                    ->html()
+                    ->searchable(['first_name', 'last_name']),
+                // Tables\Columns\TextColumn::make('first_name')
+                //     ->searchable(),
+                // Tables\Columns\TextColumn::make('last_name')
+                //     ->searchable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('phone_number')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('leadSource.name'),
+                Tables\Columns\TextColumn::make('pipelineStage.name'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCustomers::route('/'),
+            'create' => Pages\CreateCustomer::route('/create'),
+            'edit' => Pages\EditCustomer::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Customer;
+use App\Models\LeadSource;
+use App\Models\PipelineStage;
+use App\Models\Tag;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        User::factory()->create([
+            'name' => 'Test Admin',
+            'email' => 'admin@admin.com',
+        ]);
+
+        // Customer::factory()
+        // ->count(10)
+        // ->create();
+
+        $leadSources = [
+            'Website',
+            'Online AD',
+            'Twitter',
+            'LinkedIn',
+            'Webinar',
+            'Trade Show',
+            'Referral',
+        ];
+
+        foreach ($leadSources as $leadSource) {
+            LeadSource::create(['name' => $leadSource]);
+        }
+
+        $tags = [
+            'Priority',
+            'VIP'
+        ];
+
+        foreach ($tags as $tag) {
+            Tag::create(['name' => $tag]);
+        }
+
+        $pipelineStages = [
+            [
+                'name' => 'Lead',
+                'position' => 1,
+                'is_default' => true,
+            ],
+            [
+                'name' => 'Contact Made',
+                'position' => 2,
+            ],
+            [
+                'name' => 'Proposal Made',
+                'position' => 3,
+            ],
+            [
+                'name' => 'Proposal Rejected',
+                'position' => 4,
+            ],
+            [
+                'name' => 'Customer',
+                'position' => 5,
+            ]
+        ];
+
+        foreach ($pipelineStages as $stage) {
+            PipelineStage::create($stage);
+        }
+
+        $defaultPipelineStage = PipelineStage::where('is_default', true)->first()->id;
+        Customer::factory()->count(10)->create([
+            'pipeline_stage_id' => $defaultPipelineStage,
+        ]);
+    }
+}
